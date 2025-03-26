b459396aecb20d25ab93234feb4ea24635cd879b
Dominic Tillison <22tilldj@gmail.com>
2025-03-26 14:56:36 -0400

    Fixed Bugs Not allowing to see HTML Displayed to Dom

--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -4,21 +4,26 @@ const kids = getChildren()
 const celebrities = getCelebrities()
 
 const findCelebrityMatch = (kidObject, celebrityArray) => {
-    let celebrity = null
+    let celebrityMatch = null
 
     for (const celebrity of celebrityArray) {
-        if(celebrity.Id === )
+        for (const kid of kidObject) {
+            if(celebrity.id === kid.celebrityId){
+                celebrityMatch = kid
+            }
+        }
+    };
+        
+return celebrityMatch
     }
 
-    return celebrity
-}
 
 export const Pairings = () => {
     let html = ""
     html = "<ul>"
 
     for (const kid of kids) {
-        const kidsStar = findCelebrityMatch(kid, celebrities)
+        const kidsStar = findCelebrityMatch(kids, celebrities)
         html += `
             <li data-type="kid" data-id="${kid.celebrityId}">
                 ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.sport} star, by ${kid.wish}
